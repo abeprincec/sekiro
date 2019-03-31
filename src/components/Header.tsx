c3cb88b540e23cffda22cf6ebcd3737bf46e9d7d
Kasper Sandin <kasper.sandin@gmail.com>
2019-03-31 18:00:25 +0200

    Add noEdge header asset

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,13 +1,13 @@
 import * as React from 'react';
 import classNames from 'classnames';
-import { createStyles, Theme, WithStyles, withStyles } from '@material-ui/core';
-import { fade } from '@material-ui/core/styles/colorManipulator';
+import { createStyles, WithStyles, withStyles } from '@material-ui/core';
 import { CSSProperties } from '@material-ui/core/styles/withStyles';
 import { texture } from '../functions/texture';
 
-const variants = (theme: Theme) => ({
+const variants = {
   noEdge: {
-    background: fade(theme.palette.common.black, 0.75)
+    ...texture(require('../assets/header-background3.png')),
+    height: 43
   },
   singleEdge: {
     ...texture(require('../assets/header-background1.png')),
@@ -17,23 +17,22 @@ const variants = (theme: Theme) => ({
     ...texture(require('../assets/header-background2.png')),
     justifyContent: 'center'
   } as CSSProperties
+};
+
+const styles = createStyles({
+  header: {
+    display: 'flex',
+    flexDirection: 'row',
+    alignItems: 'center',
+    padding: `5px 20px`,
+    '&:not(:last-child)': {
+      marginBottom: 10
+    }
+  },
+  ...variants
 });
 
-const styles = (theme: Theme) =>
-  createStyles({
-    header: {
-      display: 'flex',
-      flexDirection: 'row',
-      alignItems: 'center',
-      padding: `5px 20px`,
-      '&:not(:last-child)': {
-        marginBottom: 10
-      }
-    },
-    ...variants(theme)
-  });
-
-export type HeaderVariant = keyof ReturnType<typeof variants>;
+export type HeaderVariant = keyof typeof variants;
 
 export type HeaderProps = WithStyles<typeof styles> &
   React.PropsWithChildren<{
