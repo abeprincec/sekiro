003c7515797284eb458d381313985a18e0ccb739
Kasper Sandin <kasper.sandin@gmail.com>
2019-03-31 17:27:05 +0200

    Ditch grid in favor of flex ( RIP )

--- src/components/Equipment.tsx
+++ src/components/Equipment.tsx
@@ -1,10 +1,5 @@
 import * as React from 'react';
-import {
-  createStyles,
-  Typography,
-  WithStyles,
-  withStyles
-} from '@material-ui/core';
+import { Typography } from '@material-ui/core';
 import { AppState } from '../state/AppState';
 import { Header } from './Header';
 import { Stats } from './Stats';
@@ -13,97 +8,72 @@ import { Item } from '../state/Item';
 import { ActiveSpatialMeta } from '../lib/spatial/ActiveSpatialMeta';
 import { useObserver } from 'mobx-react-lite';
 import { ItemSlotTiles } from './ItemSlotTiles';
+import { Row } from './Row';
+import { Column } from './Column';
 
-export const styles = createStyles({
-  root: {
-    width: '100%',
-    display: 'grid',
-    gridColumnGap: '20px',
-    gridRowGap: '20px',
-    gridTemplateColumns: '2fr 1fr 2fr',
-    gridTemplate: `
-      "tools arts pages" 1fr
-      "items jutsu pages" 1fr
-      "stats stats stats" 100px
-    `
-  },
-  tools: {
-    gridArea: 'tools'
-  },
-  items: {
-    gridArea: 'items'
-  },
-  arts: {
-    gridArea: 'arts'
-  },
-  jutsu: {
-    gridArea: 'jutsu'
-  },
-  pages: {
-    gridArea: 'pages'
-  },
-  stats: {
-    gridArea: 'stats'
-  }
-});
-
-export const Equipment = withStyles(styles)(
-  ({ classes }: WithStyles<typeof styles>) =>
-    useObserver(() => {
-      const { equipment } = React.useContext(AppState.Context);
-      return (
-        <div className={classes.root}>
-          <div className={classes.tools}>
-            <Header>
-              <Typography>Prosthetic Tools</Typography>
-            </Header>
-            <ItemSlotTiles
-              slots={equipment.toolSlots}
-              available={equipment.tools}
-            />
-          </div>
-          <div className={classes.items}>
-            <Header>
-              <Typography>Quick Items</Typography>
-            </Header>
-            <ItemSlotTiles
-              slots={equipment.itemSlots}
-              available={equipment.items}
-            />
-          </div>
-          <div className={classes.arts}>
-            <Header>
-              <Typography>Combat Arts</Typography>
-            </Header>
-            <ItemSlotTiles
-              slots={equipment.artSlots}
-              available={equipment.arts}
-              tileProps={{
-                size: 'large',
-                variant: 'octagon'
-              }}
-            />
-          </div>
-          <div className={classes.jutsu}>
-            <Header>
-              <Typography>Ninjutsu</Typography>
-            </Header>
-            <ItemSlotTiles
-              slots={equipment.ninjutsuSlots}
-              available={equipment.ninjutsu}
-              tileProps={{
-                size: 'large',
-                variant: 'octagon'
-              }}
-            />
-          </div>
+export const Equipment = () =>
+  useObserver(() => {
+    const { equipment } = React.useContext(AppState.Context);
+    return (
+      <Column flex={1}>
+        <Row flex={1}>
+          <Column flex={5}>
+            <Row flex={1}>
+              <Column flex={3}>
+                <Header>
+                  <Typography>Prosthetic Tools</Typography>
+                </Header>
+                <ItemSlotTiles
+                  slots={equipment.toolSlots}
+                  available={equipment.tools}
+                />
+              </Column>
+              <Column flex={1}>
+                <Header>
+                  <Typography>Combat Arts</Typography>
+                </Header>
+                <ItemSlotTiles
+                  slots={equipment.artSlots}
+                  available={equipment.arts}
+                  tileProps={{
+                    size: 'large',
+                    variant: 'octagon'
+                  }}
+                />
+              </Column>
+            </Row>
+            <Row flex={1}>
+              <Column flex={3}>
+                <Header>
+                  <Typography>Quick Items</Typography>
+                </Header>
+                <ItemSlotTiles
+                  slots={equipment.itemSlots}
+                  available={equipment.items}
+                />
+              </Column>
+              <Column flex={1}>
+                <Header>
+                  <Typography>Ninjutsu</Typography>
+                </Header>
+                <ItemSlotTiles
+                  slots={equipment.ninjutsuSlots}
+                  available={equipment.ninjutsu}
+                  tileProps={{
+                    size: 'large',
+                    variant: 'octagon'
+                  }}
+                />
+              </Column>
+            </Row>
+          </Column>
           <ActiveSpatialMeta type={Item}>
-            {item => <ItemPages item={item} />}
+            {item => <ItemPages item={item} style={{ flex: 3 }} />}
           </ActiveSpatialMeta>
-          <AppState.Context.Consumer>
-            {({ stats }) => <Stats className={classes.stats} stats={stats} />}
-          </AppState.Context.Consumer>
-        </div>
-      );
-    })
-);
+        </Row>
+        <AppState.Context.Consumer>
+          {({ stats }) => <Stats stats={stats} />}
+        </AppState.Context.Consumer>
+      </Column>
+    );
+  });
